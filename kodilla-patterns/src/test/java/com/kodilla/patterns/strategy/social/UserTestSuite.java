de4270b21edad2f490f52e55f1c8a02765f5f5a1
grzegorztata <grzegorz.tata@gmail.com>
2018-08-29 22:31:56 +0100

    Zad 11.3

--- kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -1,4 +1,40 @@
 package com.kodilla.patterns.strategy.social;
 
+import com.kodilla.patterns.strategy.social.socialMedia.TwitterPublisher;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserTestSuite {
+    @Test
+    public void testDefaultSharingStrategies() {
+        //Given
+        User kid = new ZGeneration("Young User");
+        User teen = new YGeneration("Teen User");
+        User adult = new Millenials("Adult User");
+        //When
+        String kidPostsOn = kid.sharePost();
+        System.out.println("Kids post on " + kidPostsOn);
+        String teenPostsOn = teen.sharePost();
+        System.out.println("Teens post on " + teenPostsOn);
+        String adultPostsOn = adult.sharePost();
+        System.out.println("Adults post on " + adultPostsOn);
+        //Then
+        Assert.assertEquals("Snapchat", kidPostsOn);
+        Assert.assertEquals("Facebook", teenPostsOn);
+        Assert.assertEquals("Twitter", adultPostsOn);
+    }
+
+    @Test
+    public void testIndividualSharingStrategy() { //zmiana upodobań (setUserMedia)
+        //Given
+        User kid = new ZGeneration("Young User");
+        //When
+        String kidPostOn = kid.sharePost();
+        System.out.println("Kids don't post on " + kidPostOn + " now.");
+        kid.setUserMedia(new TwitterPublisher());
+        kidPostOn = kid.sharePost();
+        System.out.println("Kids post on " + kidPostOn);
+        //Then
+        Assert.assertEquals("Twitter", kidPostOn);
+    }
 }
