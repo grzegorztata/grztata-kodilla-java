ac586b22826f36ce2c5404a34b9c804a8fcbf31c
grzegorztata <grzegorz.tata@gmail.com>
2018-08-29 22:30:06 +0100

    Zad 11.3

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns.strategy.social;
+
+public class User {
+    String userName;
+    protected SocialPublisher socialPublisher;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String sharePost() {
+        return socialPublisher.share();
+    }
+    public void setUserMedia(SocialPublisher socialPublisher) {
+        this.socialPublisher = socialPublisher;
+    }
+}
