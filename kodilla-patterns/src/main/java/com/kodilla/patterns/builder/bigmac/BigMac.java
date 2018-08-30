e75ccb2af59dae3d5267596c9f906c2d20098b0b
grzegorztata <grzegorz.tata@gmail.com>
2018-08-30 18:07:16 +0100

    Builder

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/BigMac.java
@@ -0,0 +1,9 @@
+package com.kodilla.patterns.builder.bigmac;
+
+public class BigMac {
+}
+
+
+// powtórzyć mod. 8. - wyjątki *określony wybór*
+// bun - z sezamem lub bez
+// sauce - standard, 1000islands, barbecue
